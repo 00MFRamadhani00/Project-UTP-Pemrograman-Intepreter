078126b6b2081b4cb8679b19ff6e63e428d2d8d3
Muhammad Fadhilah Ramadhani <92555706+00MFRamadhani00@users.noreply.github.com>
2022-10-19 22:31:27 +0700

    Update UTP.py

--- UTP.py
+++ UTP.py
@@ -8,23 +8,37 @@ while True:
     \t2. Laptop
     \t3. Service
     \t4. Exit Program''')
-    pilih = eval(input("Masukan Pilihan Anda: "))
+    pilih = input("Masukan Pilihan Anda : ")
 
     if pilih == 1 :
-        print("Satu")
-        '''
+        while next == True:
+            print("Part apa yang anda cari?")
+            print('''1. VGA
+            2. Motherboard
+            3. RAM
+            4. Processor
+            5. ''')
+            part = eval(input("Silahkan Pilih : "))
+            #Ntar disini pake collection isinya part computer
+            next = input('''Ada lagi?
+            Y untuk lanjut''')
+            if next == "Y":
+                next == True
+            else:
+                next == False
+            #Entah bagaimana ini tidak mau terbaca *emot batu*
         
-        '''
+
     elif pilih == 2 :
         print("Dua")
         '''
-
         '''
     elif pilih == 3 :
         print("Tiga")
         '''
-
         '''
     elif pilih == 4 :
         print("Terima Kasih Telah Menggunakan Program Kami")
         break
+    else:
+        print("Maaf Pilihan Tidak Tersedia")
