db05d26c9c06a219728532850b635612fd4fe59d
FerliMalkanAmien <92583458+FerliMalkanAmien@users.noreply.github.com>
2022-10-19 21:25:30 +0700

    Update UTP.py
    Penambahan modifikasi do while loop

--- UTP.py
+++ UTP.py
@@ -2,7 +2,29 @@ print("Selamat datang di Toko Komputer Kece")
 
 nama = str(input("Masukkan Nama Anda : "))
 
-print('''\tApa yang anda cari?
-\t1. Part PC
-\t2. Laptop
-\t3. Service''')
+while True:
+    print('''\tApa yang anda cari?
+    \t1. Part PC
+    \t2. Laptop
+    \t3. Service
+    \t4. Exit Program''')
+    pilih = eval(input("Masukan Pilihan Anda: "))
+
+    if pilih == 1 :
+        print("Satu")
+        '''
+        
+        '''
+    elif pilih == 2 :
+        print("Dua")
+        '''
+
+        '''
+    elif pilih == 3 :
+        print("Tiga")
+        '''
+
+        '''
+    elif pilih == 4 :
+        print("Terima Kasih Telah Menggunakan Program Kami")
+        break
