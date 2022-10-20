8866727634dea1acad2ddb505052013f9b91feca
FerliMalkanAmien <92583458+FerliMalkanAmien@users.noreply.github.com>
2022-10-20 16:46:23 +0700

    Update UTP.py

--- UTP.py
+++ UTP.py
@@ -7,6 +7,7 @@ nama = str(input("Masukkan Nama Anda : "))
 
 belanjaan = []
 thisdict = {1:"VGA", 2:"Motherboard", 3:"RAM", 4:"Processor"}
+laptopdict = {1:"Asus", 2:"Acer", 3:"HP", 4:"Lenovo", 5:"Dell"}
 
 while True:
     print('''\tApa yang anda cari?
@@ -18,11 +19,13 @@ while True:
 
     if pilih == 1 :
         
-        print('''1. VGA
+        print('''
+        1. VGA
         2. Motherboard
         3. RAM
         4. Processor
-        5. ''')
+        5. PSU
+        ''')
         berapa = eval(input("Berapa barang yang ingin anda beli?"))
 
         for i in range(berapa):
@@ -32,14 +35,36 @@ while True:
         print(belanjaan)
         
     elif pilih == 2 :
-        print("Dua")
-        '''
-        '''
+        print('''
+        1. Asus
+        2. Acer
+        3. HP
+        4. Lenovo
+        5. Dell
+        ''')
+        berapa = eval(input("Berapa barang yang ingin anda beli?"))
+
+        for i in range(berapa):
+            part = int(input("Silahkan Pilih : "))
+            belanjaan.append(laptopdict[part])
+
+        print(belanjaan)
 
     elif pilih == 3 :
-        print("Tiga")
-        '''
-        '''
+        print('''
+        1. Instal Ulang         6. Ganti RAM
+        2. Flash BIOS           7. Ganti Hardisk
+        3. Ganti Keyboard       
+        4. Ganti LCD
+        5. Ganti Thermal Paste
+        ''')
+        berapa = eval(input("Berapa barang yang ingin anda beli?"))
+
+        for i in range(berapa):
+            part = int(input("Silahkan Pilih : "))
+            belanjaan.append(laptopdict[part])
+
+        print(belanjaan)
 
     elif pilih == 4 :
         print("Terima Kasih Telah Menggunakan Program Kami")
