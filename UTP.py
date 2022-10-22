8c02fc8a643dec0c65e9ac3e400081463fe4f5fb
waliid ilham <92657818+waliidilham@users.noreply.github.com>
2022-10-22 19:57:46 +0700

    Update UTP.py

--- UTP.py
+++ UTP.py
@@ -1,21 +1,20 @@
+def belanja():
+    for i in belanjaan:
+                print(hargadict[i])
+                total = total + hargadict[i]
 
 
 print("Selamat datang di Toko Komputer Kece")
 
 nama = str(input("Masukkan Nama Anda : "))
 
-print("==================================")
-print("\tHallo", nama)
-print("==================================")
+print('==================================')
+print('========= Hai..' nama '===========')
+print('==================================')
+
+
 
 total = 0
-
-def belanja(total):
-    for i in belanjaan:
-        print(hargadict[i])
-        total = total + hargadict[i]
-    print("Total pembayaran Rp", total*Diskon)
-
 belanjaan = []
 thisdict = {1:"VGA", 2:"Motherboard", 3:"RAM", 4:"Processor", 5:"PSU"}
 laptopdict = {1:"Asus", 2:"Acer", 3:"HP", 4:"Lenovo", 5:"Dell"}
@@ -120,14 +119,19 @@ while True:
                 else :
                     print("Nomor ID tidak terdaftar")
             
-            belanja(total)
+
+            belanja()
+        
+            print("Total pembayaran Rp", total*Diskon)
             
             break
+
 
         else:    
-            belanja(total)
+            belanja()
+        
+            print("Total pembayaran Rp", total*Diskon)
             
             break
-
     else:
         print("Maaf Pilihan Tidak Tersedia")
